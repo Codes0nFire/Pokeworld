ee1f94eff9f5584598159c955c52d18d3955554b
NoN Responsive WebDeveloper <gdbcodes@gmail.com>
2024-10-27 15:09:02 +0530

    project exit

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Context, { Pokemoncontext } from './utils/Context.jsx'
+import Context from './utils/Context.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 createRoot(document.getElementById('root')).render(
   <Context>
